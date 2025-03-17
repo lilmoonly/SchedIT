5738fed40a8532d0665945349c1f21c30bf0e5df
lilmoonly <bohdan.muliarchyk@lnu.edu.ua>
2025-03-17 22:32:49 +0200

    Add EditClassroomController

---
+++ SchedIT/Controllers/EditClassroomController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyMvcApp.Data;
+using MyMvcApp.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMvcApp.Controllers
+{
+    public class EditClassroomController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EditClassroomController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var classroom = await _context.Classrooms.FindAsync(id);
+            if (classroom == null)
+            {
+                return NotFound();
+            }
+            return View(classroom);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Classroom classroom)
+        {
+            if (id != classroom.Id)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(classroom);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Classroom");
+            }
+            return View(classroom);
+        }
+    }
+}
