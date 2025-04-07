66e344307466868d5d0a6173ef969b280d8729d6
MarkiyanAndrusevych <markiyan@racse.org.ua>
2025-04-07 21:48:51 +0300

    Added some tests

--- SchedIT/Migrations/20250407175933_InitialCreate.Designer.cs
+++ SchedIT/Migrations/20250407175933_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using MyMvcApp.Data;
 namespace MyMvcApp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20250401075847_InitialCreate")]
+    [Migration("20250407175933_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
